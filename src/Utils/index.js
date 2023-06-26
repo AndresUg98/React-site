841bad56f58099f8196cdf0c00b4dfebeb11fa12
AndresUg98 <oscaran2109@gmail.com>
2023-06-25 21:15:38 -0700

    total amount fixed

--- src/Utils/index.js
+++ src/Utils/index.js
@@ -1,4 +1,10 @@
 export const totalPrice = (products) =>{
+    /**
+     * This function calculates the total price of an order
+     * 
+     * @param {Array} products cartProducts: Array of objects
+     * @returns {number} Total price 
+     */
     let sum = 0;
     products.forEach(product => sum += product.price);
     return sum;
